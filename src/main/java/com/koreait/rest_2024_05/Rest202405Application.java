4712c2e67bf4cf907e2c18af61b5689778a9a3e5
yunlinIT <yunlinit@gmail.com>
2024-05-14 11:15:15 +0900

    프로젝트 생성 :

---
+++ src/main/java/com/koreait/rest_2024_05/Rest202405Application.java
@@ -0,0 +1,13 @@
+package com.koreait.rest_2024_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest202405Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rest202405Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/rest_2024_05/Rest202405ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.rest_2024_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest202405ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
