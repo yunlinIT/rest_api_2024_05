3a65bd5619af9a7c3433139af1cc4394d1968345
yunlinIT <yunlinit@gmail.com>
2024-05-16 16:36:36 +0900

    memebr/me TestCase

--- src/test/java/com/koreait/rest_2024_05/boundedContext/member/MemberControllerTests.java
+++ src/test/java/com/koreait/rest_2024_05/boundedContext/member/MemberControllerTests.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -52,6 +53,25 @@ public class MemberControllerTests {
                 .andExpect(jsonPath("$.resultCode").value("S-1"))
                 .andExpect(jsonPath("$.msg").exists())
                 .andExpect(jsonPath("$.data.accessToken").exists());
+    }
+
+    @Test
+    @DisplayName("GET /member/me 는 내 정보를 조회하는 URL 이다.")
+    void t2() throws Exception {
+        // When
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/member/me")
+                )
+                .andDo(print());
+
+        // Then
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.resultCode").value("S-1"))
+                .andExpect(jsonPath("$.msg").exists())
+                .andExpect(jsonPath("$.data.member.id").exists())
+                .andExpect(jsonPath("$.data.member.username").exists());
     }
 
 }
