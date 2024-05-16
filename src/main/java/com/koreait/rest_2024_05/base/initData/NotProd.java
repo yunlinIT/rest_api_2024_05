b5b46f71dacec1b999c634221f68a2602baa01c2
yunlinIT <yunlinit@gmail.com>
2024-05-16 14:23:31 +0900

    로그인을 시도하면 응답 헤더로 액세스 토큰을 받아야한다.

--- src/main/java/com/koreait/rest_2024_05/base/initData/NotProd.java
+++ src/main/java/com/koreait/rest_2024_05/base/initData/NotProd.java
@@ -3,21 +3,11 @@ package com.koreait.rest_2024_05.base.initData;
 
 import com.koreait.rest_2024_05.member.entity.Member;
 import com.koreait.rest_2024_05.member.service.MemberService;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Configuration
 @Profile({"dev", "test"})
--- src/main/java/com/koreait/rest_2024_05/boundedContext/member/MemberController.java
+++ src/main/java/com/koreait/rest_2024_05/boundedContext/member/MemberController.java
@@ -2,6 +2,7 @@ package com.koreait.rest_2024_05.boundedContext.member;
 
 import com.koreait.rest_2024_05.member.entity.Member;
 import com.koreait.rest_2024_05.member.service.MemberService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -26,7 +27,9 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public Member login(@Valid @RequestBody LoginRequest loginRequest) {
+    public Member login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
+        resp.addHeader("Authentication","JWT 토큰");
+
         return memberService.findByUsername(loginRequest.getUsername()).orElse(null);
     }
 }
