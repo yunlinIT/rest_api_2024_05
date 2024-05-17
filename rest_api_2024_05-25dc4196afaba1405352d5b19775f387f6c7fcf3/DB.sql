009b1757da53f6451eb0d07e86e017353961c19a
yunlinIT <yunlinit@gmail.com>
2024-05-17 15:10:52 +0900

    JwtAuthorizationFilter에서 회원 찾을때 id로 검색, 속도 때문

---
+++ rest_api_2024_05-25dc4196afaba1405352d5b19775f387f6c7fcf3/DB.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS rest_api_2024_05___dev;
+CREATE DATABASE rest_api_2024_05___dev;
+USE rest_api_2024_05___dev;
--- rest_api_2024_05-25dc4196afaba1405352d5b19775f387f6c7fcf3/src/main/java/com/koreait/rest_2024_05/member/service/MemberService.java
+++ rest_api_2024_05-25dc4196afaba1405352d5b19775f387f6c7fcf3/src/main/java/com/koreait/rest_2024_05/member/service/MemberService.java
@@ -43,4 +43,8 @@ public class MemberService {
 
         return jwtProvider.genToken(member.toClaims(), 60 * 60 * 24 * 365);
     }
+
+    public Optional<Member> findById(long id) {
+        return memberRepository.findById(id);
+    }
 }
